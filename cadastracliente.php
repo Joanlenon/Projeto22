9ddb7d57be42f26cead2c6a471565a3c109bbae0
joan lenon <118912952+Joanlenon@users.noreply.github.com>
2023-06-27 23:42:41 -0300

    Update cadastracliente.php

--- cadastracliente.php
+++ cadastracliente.php
@@ -1,44 +1,44 @@
 <?php
 include("conectadb.php");
+if($_SERVER['REQUEST_METHOD']== 'POST')
+{
+    $cpf= $_POST['cpf'];
+    $nome= $_POST['nome'];
+    $senha=  $_POST['senha'];
+    $datanasc=  $_POST['datanasc'];
+    $telefone= $_POST['telefone'];
+    $logradouro= $_POST['logradouro'];
+    $numero=  $_POST['numero'];
+    $cidade= $_POST['cidade'];
 
-session_start();
+    $sql="SELECT COUNT(cli_cpf) FROM clientes WHERE cli_cpf= '$cpf'";
+    $retorno= mysqli_query($link, $sql);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nome = $_POST['nome'];
-    $senha = $_POST['senha'];
-    $cpf = $_POST['frmCpf'];
-    $datansc = $_POST['data'];
-    $telefone = $_POST['numero_telefone'];
-    $logradouro = $_POST['logradouro'];
-    $numero = $_POST['numero'];
-    $cidade = $_POST['cidade'];
-   
+    while($tbl = mysqli_fetch_array($retorno)){
+        $cont = $tbl[0];
+    }
 
-    $sql = "SELECT COUNT(cli_id) AS count FROM clientes WHERE cli_nome = '$nome' AND cli_cpf = '$cpf'";
-    $result = mysqli_query($link, $sql);
+    #VALIDAÇÃO DE TRUE E FALSE
+    if($cont == 1){
+        echo"<script>window.alert('USUARIO JÁ EXISTE');</script>";
+    }else{
+        $sql= "INSERT INTO clientes (cli_cpf, cli_nome,cli_senha, cli_datanasc, cli_telefone, cli_logradouro, cli_numero, cli_cidade, cli_ativo) VALUES ('$cpf', '$nome', '$senha', STR_TO_DATE('$datanasc', '%Y-%m-%d'),'$telefone','$logradouro', '$numero', '$cidade', 's' )";
+        mysqli_query($link, $sql);
 
-    if ($result) {
-        $row = mysqli_fetch_assoc($result);
-        $cont = $row['count'];
-
-        if ($cont == 1) {
-            echo "<script>alert('USUÁRIO JÁ EXISTE');</script>";
-        } else {
-            $insertQuery = "INSERT INTO clientes (cli_nome, cli_senha, cli_cpf, cli_datansc, cli_telefone, cli_logradouro, cli_numero, cli_cidade, cli_ativo)
-                VALUES ('$nome', '$senha', '$cpf', '$datansc', '$telefone', '$logradouro', '$numero', '$cidade', 's')";
-            $insertResult = mysqli_query($link, $insertQuery);
-
-            if ($insertResult) {
-                echo "<script>alert('CLIENTE CADASTRADO');</script>";
-                echo "<script>window.location.href='listacliente.php';</script>";
-            } else {
-                echo "<script>alert('Erro ao cadastrar cliente.');</script>";
-            }
-        }
-    } else {
-        echo "<script>alert('Erro ao consultar banco de dados.');</script>";
+        echo"<script>window.alert('CLIENTE CADASTRADO');</script>";
+            echo"<script>window.location.href='admhome.php';</script>";
     }
 }
+#cli_id	
+#cli_cpf
+#cli_nome
+#cli_senha
+#cli_datanasc	
+#cli_telefone	
+#cli_logradouro	
+#cli_numero	
+#cli_cidade	
+#cli_ativo	
 ?>
 
 <!DOCTYPE html>
@@ -48,42 +48,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estiloadm.css">
-    <title>Cadastro de Cliente</title>
+    <title>CADASTRO DE CLIENTE</title>
 </head>
 
 <body>
     <div>
         <ul class="menu">
-            <li><a href="cadastrousuario.php">CADASTRA USUÁRIO</a></li>
+        <li><a href="cadastrausuario.php">CADASTRA USUÁRIO</a></li>
             <li><a href="listausuario.php">LISTA USUÁRIO</a></li>
-            <li><a href="cadastraproduto.php">CADASTRA PRODUTO</a></li>
-            <li><a href="cadastraclientes.php">CADASTRA CLIENTES</a></li>
-            <li><a href="listaproduto.php">LISTA PRODUTO</a></li>
+            <li><a href="cadastracliente.php">CADASTRA CLIENTE</a></li>
             <li><a href="listacliente.php">LISTA CLIENTE</a></li>
-            <li class="menuloja"><a href="./areacliente/loja.php">LOJA</a></li>
+            <li class="menuloja"><a href="logout.php">SAIR</a></li>
         </ul>
     </div>
+
     <div>
         <form action="cadastracliente.php" method="post">
-            <input type="text" name="nome" id="nome" placeholder="NOME USUÁRIO" required>
+            <!--placeholder é o nome que vai aparecer dentro do text do input-->
+            <input type="number" name="cpf" id="cpf" placeholder="CPF">
             <br>
-            <input type="password" name="senha" id="senha" placeholder="Senha" required>
+            <input type="text" name="nome" id="nome" placeholder="NOME USUARIO">
             <br>
-            <input type="text" id="frmCpf" name="frmCpf" maxlength="11" size="11" placeholder="CPF" required>
+            <input type="password" name="senha" id="senha" placeholder="SENHA">
             <br>
-            <input type="date" name="data" id="data" placeholder="Data Nasc">
+            <input type="date" name="datanasc" id="datanasc" placeholder="DATA DE NASCIMENTO">
             <br>
-            <input type="text" name="numero_telefone" id="numero_telefone" placeholder="Número telefone">
+            <input type="number" name="telefone" id="telefone" placeholder="TELEFONE">
             <br>
-            <input type="text" name="logradouro" id="logradouro" placeholder="Logradouro">
+            <input type="text" name="logradouro" id="logradouro" placeholder="LOGRADOURO">
             <br>
-            <input type="number" name="numero" id="numero" placeholder="Número">
+            <input type="text" name="numero" id="numero" placeholder="NÚMERO LOGRADOURO">
             <br>
-            <input type="text" name="cidade" id="cidade" placeholder="Cidade">
+            <input type="text" name="cidade" id="cidade" placeholder="CIDADE">
             <br>
-            <input type="submit" name="cadastro" id="cadastrar" value="CADASTRAR">
+            <input type="submit" name="cadastrar" id="cadastrar" value="CADASTRA">
         </form>
     </div>
+
 </body>
 
 </html>
