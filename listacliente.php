7a3cacbb359a3b1907e4f95773fe7480d427361c
joan lenon <118912952+Joanlenon@users.noreply.github.com>
2023-06-28 11:36:58 -0300

    Update listacliente.php

--- listacliente.php
+++ listacliente.php
@@ -1,21 +1,29 @@
 <?php
 include("conectadb.php");
-session_start();
-$nomeusuario = $_SESSION['nomeusuario'];
-if (!$nomeusuario) {
-    echo "<script>window.alert('USUARIO NÃO AUTENTICADO');window.location.href='login.php';</script>";
-    exit();
-}
+    session_start();
+    $nomeusuario = $_SESSION['nomeusuario'];
 
-$ativo = isset($_POST['ativo']) ? $_POST['ativo'] : 's';
+#JÁ LISTA OS USUÁRIOS DO MEU BANCO
 
-if ($ativo === 's') {
-    $sql = "SELECT * FROM clientes WHERE cli_ativo = 's'";
-} else {
-    $sql = "SELECT * FROM clientes WHERE cli_ativo = 'n'";
-}
-
+$sql = "SELECT * FROM clientes WHERE cli_ativo = 's'";
 $retorno = mysqli_query($link, $sql);
+
+#JÁ FORÇA TRAZER NA VARIÁVEL ATIVO
+$ativo = 's';
+
+# COLETA O BOTÃO DE POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $ativo = $_POST['ativo'];
+
+    #VERIFICA SE USUÁRIO ESTÁ ATIVO PARA A LISTA
+    if ($ativo == 's') {
+        $sql = "SELECT * FROM clientes WHERE cli_ativo = 's'";
+        $retorno = mysqli_query($link, $sql);
+    } else {
+        $sql = "SELECT * FROM clientes WHERE cli_ativo = 'n'";
+        $retorno = mysqli_query($link, $sql);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -25,46 +33,90 @@ $retorno = mysqli_query($link, $sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estiloadm.css">
-    <title>Lista Cliente</title>
+    <title>Lista USUÁRIOS</title>
+
 </head>
 
 <body>
     <div>
         <ul class="menu">
-            <li><a href="cadastrousuario.php">CADASTRA USUÁRIO</a></li>
+        <li><a href="cadastrausuario.php">CADASTRA USUÁRIO</a></li>
+            
             <li><a href="listausuario.php">LISTA USUÁRIO</a></li>
             <li><a href="cadastraproduto.php">CADASTRA PRODUTO</a></li>
-            <li><a href="cadastracliente.php">CADASTRA CLIENTE</a></li>
+            <li><a href="cadastracliente.php">CADASTRA CLIENTES</a></li>
             <li><a href="listaproduto.php">LISTA PRODUTO</a></li>
             <li><a href="listacliente.php">LISTA CLIENTE</a></li>
             <li class="menuloja"><a href="./logout.php">SAIR</a></li>
             <li class="profile">Olá <?= strtoupper($nomeusuario) ?></li>
+            <?php
+            #ABERTO O PHP PARA VALIDAR SE A SESSÃO DO USUARIO ESTÁ ABERTA
+            # SE SESSÃO ABERTA, FECHA O PHP PARA USAR ELEMENTOS HTML
+            if ($nomeusuario != null) {
+                ?>
+                <!--USO DE ELEMENTO HTML COM PHP INTERNO-->
+                <li class="profile">Olá
+                    <?= strtoupper($nomeusuario) ?>
+                </li>
+                <?php
+                # ABERTURA DE OUTRO PHP PARA CASO FALSE
+            } else {
+                echo "<script>window.alert('USUARIO NÃO AUTENTICADO'); window.location.href='login.php';</script>";
+            }
+            # FIM DO PHP PARA CONTINUAR MEU HTML
+            ?>
         </ul>
     </div>
-
+    
+    <!--AQUI LISTA OS USUÁRIOS DO BANCO-->
     <div id="background">
         <form action="listacliente.php" method="post">
-            <input type="radio" name="ativo" class="radio" value="s" required onchange="this.form.submit()" <?= $ativo === 's' ? 'checked' : '' ?>>ATIVOS
-            <input type="radio" name="ativo" class="radio" value="n" required onchange="this.form.submit()" <?= $ativo === 'n' ? 'checked' : '' ?>>INATIVOS
+            <input type="radio" name="ativo" class="radio" value="s" required onclick="submit()" 
+            <?=$ativo == 's'? "checked":""?>>ATIVOS
+
+            <input type="radio" name="ativo" class="radio" value="n" required onclick="submit()" 
+            <?=$ativo == 'n'? "checked":""?>>INATIVOS
+
         </form>
 
         <div class="container">
             <table border="1">
                 <tr>
                     <th>NOME</th>
+                    <th>CPF</th>
+                    <th>DATA DE NASCIMENTO</th>
+                    <th>TELEFONE</th>
+                    <th>LOGRADOURO</th>
+                    <th>NÚMERO</th>
+                    <th>CIDADE</th>
                     <th>ALTERAR DADOS</th>
                     <th>ATIVO?</th>
                 </tr>
-                <?php while ($tbl = mysqli_fetch_array($retorno)) { ?>
+
+                <!--BRUXARIA EM PHP-->
+                <?php
+                    while($tbl = mysqli_fetch_array($retorno)){
+                ?>
                     <tr>
-                        <td><?= $tbl[1] ?></td>
-                        <td><a href="alteracliente.php?id=<?= $tbl[0] ?>"><input type="button" value="ALTERAR DADOS"></a></td>
-                        <td><?= $tbl[3] === 's' ? 'SIM' : 'NÃO' ?></td>
+                        <td><?= $tbl[1]?></td> <!--TRAZ SOMENTE A COLUNA 1 DO BANCO [NOME]-->
+                        <td><?= $tbl[2]?></td>
+                        <td><?= $tbl[4]?></td>
+                        <td><?= $tbl[5]?></td>
+                        <td><?= $tbl[6]?></td>
+                        <td><?= $tbl[7]?></td>
+                        <td><?= $tbl[8]?></td>
+                        <td><a href="alteracliente.php?id=<?=$tbl[0]?>"><input type="button" value="ALTERAR DADOS"></a></td> 
+                        <td><?=$check = ($tbl[9] == 's')?"SIM":"NÃO"?></td><!--VALIDA S OU N E ESCREVE "SIM" E "NÃO"-->
                     </tr>
-                <?php } ?>
+                    <?php 
+                    }
+                    ?>
+                
+            
             </table>
         </div>
     </div>
+
 </body>
 
 </html>
