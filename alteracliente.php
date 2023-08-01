b6341bc462564d3aa05bd9946598a451fbc6db43
Joanlenon <118912952+Joanlenon@users.noreply.github.com>
2023-08-01 11:48:55 -0300

    hoje foi elaborado o ver produto
    Foi feito em aula o ver produto e suas funções em php

--- alteracliente.php
+++ alteracliente.php
@@ -11,8 +11,8 @@ $retorno = mysqli_query($link, $sql);
 
 #PREENCHA O ARRAY SEMPRE. enquanto tiver dados ele busca e joga nos campos precisos
 while ($tbl = mysqli_fetch_array($retorno)) {
-    $nome = $tbl[2]; #CAMPO NOME DA TABELA DO BANCO
-    $cpf = $tbl[1]; #CAMPO CPF DA TABELA DO BANCO
+    $nome = $tbl[1]; #CAMPO NOME DA TABELA DO BANCO
+    $cpf = $tbl[2]; #CAMPO CPF DA TABELA DO BANCO
     $senha = $tbl[3]; #CAMPO SENHA DA TABELA DO BANCO
     $datanasc = $tbl[4]; #CAMPO DATANASC DA TABELA DO BANCO
     $telefone = $tbl[5]; #CAMPO TELEFONE DA TABELA DO BANCO
@@ -28,14 +28,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nome = $_POST['nome'];
     $cpf = $_POST['cpf'];
     $senha = $_POST['senha'];
-    $datanasc = $_POST['datanasc'];
+    $datansc = $_POST['datansc'];
     $telefone = $_POST['telefone'];
     $logradouro = $_POST['logradouro'];
     $numero = $_POST['numero'];
     $cidade = $_POST['cidade'];
     $ativo = $_POST['ativo'];
 
-    $sql = "UPDATE clientes SET cli_nome = '$nome', cli_cpf = '$cpf', cli_senha = '$senha', cli_datanasc = STR_TO_DATE('$datanasc','%Y-%m-%d'), cli_telefone = '$telefone', cli_logradouro = '$logradouro', cli_numero = '$numero', cli_cidade = '$cidade', cli_ativo = '$ativo' WHERE cli_id = $id";
+    $sql = "UPDATE clientes SET cli_nome = '$nome', cli_cpf = '$cpf', cli_senha = '$senha', cli_datansc = STR_TO_DATE('$datansc','%Y-%m-%d'), cli_telefone = '$telefone', cli_logradouro = '$logradouro', cli_numero = '$numero', cli_cidade = '$cidade', cli_ativo = '$ativo' WHERE cli_id = $id";
 
     mysqli_query($link,$sql);
 
@@ -70,7 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <!--placeholder é o nome que vai aparecer dentro do text do input-->
 
             <input type="hidden" name="id" value="<?=$id?>">
-            <input type="text" name="nome" id="nome" value="<?=$nome ?>" required>  <!-- dentro do php ele vai puxar a informação do banco -->
+            <input type="text" name="nome" id="nome" value="<?=$nome ?>"required> <!-- dentro do php ele vai puxar a informação do banco -->
             <br>
             <input type="number" name="cpf" id="cpf" value="<?=$cpf ?>" required>
             <br>
