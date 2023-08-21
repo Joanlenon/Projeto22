f9bd3df98604de694c67f3e77f2e1943e09089f8
Joanlenon <118912952+Joanlenon@users.noreply.github.com>
2023-08-21 09:29:35 -0300

    atualização de código

--- areacliente/loja.php
+++ areacliente/loja.php
@@ -1,49 +1,43 @@
 <?php
 include("../conectadb.php");
-session_start();
 
+session_start();
 $sql = "SELECT * FROM produtos WHERE pro_ativo = 's'";
 $retorno = mysqli_query($link, $sql);
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="pt-br>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/loja.css">
     <title>LOJA PRODUTOS</title>
 </head>
-
 <body>
-    <!--MENU DA LOJA -->
+    <!-- MENÚ DA LOJA -->
     <header>
         <nav>
             <ul class="menu">
-                <li><a href="loja.php">HOME</a></li><!--BOTÃO HOME-->
-                <li><a href="#"></a>PRODUTOS</li>
+                <li><a href="loja.php">HOME</li></a> <!--BOTÃO HOME -->
+                <li><a href="#">PRODUTOS</li></a>
 
+                <!-- VALIDA SE O CLIENTE LOGOU -->
                 <?php
-                if (isset($_SESSION['idcliente'])) {
+                if (isset($_SESSION['idcliente'])){
                 ?>
                     <form class="formmenu" action="logout.php" method="post">
-                        <h3 class="menu-right2">
-                            olá <?= $_SESSION['nomecliente']; ?>
-                        </h3>
-                        <li class="menu-right">
-                            <a href="perfil.php?id=<?= $sessao_cliente ?>">PERFIL</a>
-                        </li>
-                        <li class="menu-right">
-                            <a href="logout.php">LOGOUT</a>
-                        </li>
+                        <h3 class="menu-right2">Olá <?= $_SESSION['nomecliente'];?></h3>
+                        <li class="menu-right"><a href="perfil.php?id=<?=$sessao_idcliente?>">PERFIL</a></li>
+                        <li class="menu-right"><a href="logout.php">LOGOUT</li></a>
                     </form>
                 <?php
-                } else {
+                }    
+                else {
                 ?>
-                    <form class="formmenu2">
+                    <form class="formenu2">
                         <li class="menu-right"><a href="logincliente.php" style="float: right">ENTRAR</a></li>
-                        <li class="menu-right"><a href="../cadastracliente.php">CADASTRA</a></li>
+                        <li class="menu-right"><a href="cadastracliente.php">CADASTRAR</a></li>
                     </form>
                 <?php
                 }
@@ -51,37 +45,28 @@ $retorno = mysqli_query($link, $sql);
             </ul>
         </nav>
     </header>
-    <!---CORPO DA LOJA--->
+
+    <!-- CORPO DA LOJA -->
     <div class="conteudo">
         <?php
-        while ($tbl = mysqli_fetch_array($retorno)) {
-        ?>
+            while($tbl = mysqli_fetch_array($retorno)){
+            ?>
             <div class="card">
-                <!-- COLETA A IMAGEM DO PRODUTOS-->
-                <div class="image-container">
-                    <img src="data:image/jpeg;base64, <?= $tbl[7] ?>" style="width: 300px; height:300px;
-                     
-                     ">
-                </div>
-                <!-- NOME DO PRODUTOS-->
-                <h1 class="tituloprod"><?= $tbl[1] ?></h1>
-                <!--PREÇO DO PRODUTO-->
-                <p class="price">R$<?= number_format($tbl[5], 2, ',', '.') ?></p>
-                <!--DESCRIÇÃO DO PRODUTO-->
-                <p><?= $tbl[2] ?></p>
-
-                <!--BOTÃO DO CARRINHO-->
-                <p><a class="botao" href="verproduto.php" ?id=<?= $tbl[0] ?>">
-                        <input type="button" class="botao" value="VISUALIZAR DETALHES"></a></p>
+                <!-- COLETA IMAGEM DO PRODUTO -->
+                <img src="data:image/jpeg;base64, <?= $tbl[7]?>" style="width: 300px; height: 300px;">
+                <!-- NOME DO PRODUTO -->
+                <h1 class="tituloprod"><?=$tbl[1]?></h1>
+                <!-- PREÇO DO PROUDTO -->
+                <p class="price">R$ <?= number_format($tbl[5],2,',','.')?></p>
+                <!-- DESCRIÇÃO DO PRODUTO -->
+                <p><?=$tbl[2]?></p>
+                <!-- BOTÃO ADD PRODUTO NO CARRINHO -->
+                <p><a class="botao" href="verproduto.php?id=<?=$tbl[0]?>">
+                <input type="button" class="botao" value="VISUALIZAR DETALHES"></a></p>
             </div>
-        <?php
-        }
-        ?>
-
-
+            <?php
+            }
+            ?>
     </div>
-
-
 </body>
-
 </html>
--- areacliente/verproduto.php
+++ areacliente/verproduto.php
@@ -1,166 +1,107 @@
 <?php
-
 include("../conectadb.php");
-
 session_start();
 
-
-
 //COLETA DE DADOS DE CLIENTE NA SESSÃO
-
 $idcliente = $_SESSION['idcliente'];
 
-
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $id = $_POST['id'];
-
     $nomeproduto = $_POST['nomeproduto'];
-
-    $quantidade = intval($_POST['quantidade']);
-
-    $preco = floatval($_POST['preco']);
-
-
+    $quantidade = $_POST['quantidade'];
+    $preco = $_POST['preco'];
 
     //TOTAL ITEM É A SOMA DO PREÇO UNITÁRIO * QUANTIDADE
-
     $totalitem = ($preco * $quantidade);
 
-
-
     //GERAR HASH PARA DEFINIR CARRINHO ÚNICO E EXCLUSIVO
-
-    $numerocarrinho = MD5($_SESSION['idcliente'] . date('d-m-Y H-i-s')); //MD5 criptografia hash
-
-
+    $numerocarrinho = MD5($_SESSION['idcliente']. date('d-m-Y H-i-s')); //MD5 criptografia hash
 
     //VALIDA CLIENTE
-
-    if ($idcliente <= 0) {
-
-        echo "<script>window.alert('VOCÊ PRECISA LOGAR PARA ADICIONAR ITEM AO CARRINHO');</script>";
-
-        echo "<script>window.location.href='loja.php';</script>";
-    } else {
-
+    if($idcliente <= 0){
+        echo"<script>window.alert('VOCÊ PRECISA LOGAR PARA ADICIONAR ITEM AO CARRINHO');</script>";
+        echo"<script>window.location.href='loja.php';</script>";
+    }
+    else{
         //VERIFICA SE O CLIENTE TEM CARRINHO ABERTO
-
         $sql = "SELECT COUNT(car_numero_carrinho) FROM itens_carrinho INNER JOIN clientes ON fk_cli_id = cli_id WHERE cli_id = '$idcliente' AND car_finalizado = 'n'"; //verifica quantos carrinhos esse cliente tem e se tem finalizado
+        $retorno = mysqli_query($link,$sql);
 
-        $retorno = mysqli_query($link, $sql);
-
-
-
-        while ($tbl = mysqli_fetch_array($retorno)) {
-
+        while($tbl = mysqli_fetch_array($retorno)){
             $cont = $tbl[0];
-
-            if ($cont == 0) {
-
+            if($cont == 0){
                 //SE O CARRINHO NÃO EXISTE NO BANCO, GERA UM NOVO MD5 E INSERE OS ITENS NA TABELA
-
                 $sql = "INSERT INTO itens_carrinho (fk_pro_id, car_item_quantidade, fk_cli_id, car_total_carrinho, car_numero_carrinho, car_finalizado) VALUES($id, '$quantidade', '$idcliente', '$totalitem', '$numerocarrinho', 'n')";
-
                 mysqli_query($link, $sql);
-
-
-
+                
                 $_SESSION['carrinhoid'] = $numerocarrinho;
 
-
-
-                echo "<script>window.alert('PRODUTO ADICIONADO AO CARRINHO');</script>";
-
-                echo "<script>window.location.href='loja.php';</script>";
-            } else {
-
+                echo"<script>window.alert('PRODUTO ADICIONADO AO CARRINHO');</script>";
+                echo"<script>window.location.href='loja.php';</script>";
+            }
+            else{
                 //SE O CARRINHO EXISTE, CONSULTA O NÚMERO DO CARRINHO PARA ADICIONAR ITENS AO CARRINHO
-
                 $sql = "SELECT DISTINCT(car_numero_carrinho) FROM itens_carrinho WHERE fk_cli_id ='$idcliente' AND car_finalizado = 'n'";
-
-                $retorno = mysqli_query($link, $sql);
-
-
-
-                while ($tbl = mysqli_fetch_array($retorno)) {
-
+                $retorno = mysqli_query($link,$sql);
+                
+                while($tbl = mysqli_fetch_array($retorno)){
                     $numerocarrinhocliente = $tbl[0];
-
                     $_SESSION['carrinhoid'] = $numerocarrinhocliente;
-
                     $sql = "INSERT INTO itens_carrinho (fk_pro_id, car_item_quantidade, fk_cli_id, car_total_carrinho, car_numero_carrinho, car_finalizado) VALUES($id, '$quantidade', '$idcliente', '$totalitem', '$numerocarrinhocliente', 'n')";
 
-
-
-                    mysqli_query($link, $sql);
-
-                    echo "<script>window.alert('PRODUTO ADICIONADO AO CARRINHO $numerocarrinhocliente');</script>";
-
-                    echo "<script>window.location.href='loja.php';</script>";
+                    mysqli_query($link,$sql);
+                    echo"<script>window.alert('PRODUTO ADICIONADO AO CARRINHO $numerocarrinhocliente');</script>";
+                    echo"<script>window.location.href='loja.php';</script>";
                 }
             }
         }
     }
 }
+
 //TRAZENDO DADOS VIA URL(GET)
-
-$id = isset($_POST['id']) ? $_POST['id'] : '';
-
-
+$id = $_GET['id'];
 $sql = "SELECT * FROM produtos WHERE pro_id = '$id'";
 
-
-
-$retorno = mysqli_query($link, $sql);
-
-while ($tbl = mysqli_fetch_array($retorno)) {
-
+$retorno = mysqli_query($link,$sql);
+while($tbl = mysqli_fetch_array($retorno)){
     $nomeproduto = $tbl[1];
-
     $descricao = $tbl[2];
-
     $preco = $tbl[5];
-
     $imagem_atual = $tbl[7];
 }
-
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/estiloadm.css">
     <title>VER PRODUTO</title>
 </head>
-
 <body>
     <!-- NOSSO MENU GLOBAL -->
     <!-- MENÚ DA LOJA -->
     <header>
         <nav>
             <ul class="menu">
-                <li><a href="loja.php">HOME</a></li><!-- BOTÃO HOME -->
-                <li><a href="#">PRODUTOS</a></li>
+                <li><a href="loja.php">HOME</li></a> <!--BOTÃO HOME -->
+                <li><a href="#">PRODUTOS</li></a>
 
                 <!-- VALIDA SE O CLIENTE LOGOU -->
                 <?php
-                if (isset($_SESSION['idcliente'])) {
+                if (isset($_SESSION['idcliente'])){
                 ?>
                     <form class="formmenu" action="logout.php" method="post">
-                        <h3 class="menu-right2">Olá, <?= $_SESSION['nomecliente']; ?></h3>
-                        <li class="menu-right"><a href="perfil.php?id=<?= $_SESSION['idcliente']; ?>">PERFIL</a></li>
-                        <li class="menu-right"><a href="logout.php">LOGOUT</a></li>
+                        <h3 class="menu-right2">Olá, <?= $_SESSION['nomecliente'];?></h3>
+                        <li class="menu-right"><a href="perfil.php?id=<?=$sessao_idcliente?>">PERFIL</a></li>
+                        <li class="menu-right"><a href="logout.php">LOGOUT</li></a>
                     </form>
                 <?php
-                } else {
+                }    
+                else {
                 ?>
-                    <form class="formmenu2">
+                    <form class="formenu2">
                         <li class="menu-right"><a href="logincliente.php" style="float: right">ENTRAR</a></li>
                         <li class="menu-right"><a href="cadastracliente.php">CADASTRAR</a></li>
                     </form>
@@ -170,35 +111,34 @@ while ($tbl = mysqli_fetch_array($retorno)) {
             </ul>
         </nav>
     </header>
-    <!-- FIM MENU GLOBAL -->
 
+    <!-- FIM MENU GLOBAL -->
 
     <div class="formulario">
         <form class="visualizaproduto" action="verproduto.php" method="post" enctype="multipart/form-data">
-            <input type="hidden" name="id" id="nome" value="<?= isset($id) ? $id : '' ?>">
+            <input type="hidden" name="id" id="nome" value="<?= $id?>">
             <label>NOME</label>
-            <input type="text" name="nomeproduto" id="nomeproduto" value="<?= isset($nomeproduto) ? $nomeproduto : '' ?>" readonly>
+            <input type="text" name="nomeproduto" id="nome" value="<?= $nomeproduto?>" readonly>
 
             <label>DESCRIÇÃO</label>
-            <textarea name="descricao" readonly><?= isset($descricao) ? $descricao : '' ?></textarea>
+            <textarea name="descricao" readonly><?= $descricao?></textarea>
 
             <label>QUANTIDADE</label>
             <input type="number" id="quantidade" name="quantidade" required>
 
             <label>PREÇO</label>
-            <input type="text" id="preco" name="preco" value="<?= isset($preco) ? number_format($preco, 2, ',', '.') : '' ?>" readonly>
+            <input type="decimal" id="preco" name="preco" value="<?= $preco?>" readonly>
 
             <br>
-
             <input type="submit" value="ADICIONAR AO CARRINHO">
         </form>
     </div>
 
     <!-- PEÇA ESSENCIAL PARA COLETAR IMAGEM ATUAL -->
+
     <div>
-        <img name="imagem_atual" class="imagem_atual" src="data:image/jpeg;base64,<?= isset($imagem_atual) ? $imagem_atual : '' ?>">
+        <td><img name="imagem_atual" class="imagem_atual" src="data:image/jpeg;base64,<?= $imagem_atual?>"></img></td>
     </div>
 
 </body>
-
 </html>
